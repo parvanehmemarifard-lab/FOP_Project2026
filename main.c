2ce264e48efeb35c6aa89a32b04cd4cbcecc820b
parvanehmemarifard-lab <parvanehmemarifard@gmail.com>
2026-07-21 16:01:44 +0330

    Add files via upload

--- main.c
+++ main.c
@@ -4,10 +4,6 @@
 #include "student.h"
 
 int main(){
-    FILE* file = fopen("students.json", "ab+");
-    Student s1 = {"pari", "memari","404106377", "52641625", "d", 404, "s", " ", " ", "tehran", "hola", "red"};
-    Student s2 = {"parivaneh", "mari","4043378", "52641625", "d", 403, "s", " ", " ", "boroj", "adios", "pink"};
-    fwrite(&s1, sizeof(Student), 1, file);
-    fwrite(&s2, sizeof(Student), 1, file);
     loginPage();
+    return 0;
 }
